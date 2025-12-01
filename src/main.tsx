cf22254cbd3221292677c9de498d021a4490c703
[dyad] <git@dyad.sh>
2025-12-01 11:33:30 -0300

    [dyad] Corrigiendo el error 'React.Children.only' moviendo Toaster y Sonner a main.tsx dentro de un Fragment y simplificando la raíz de App.tsx. - wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import React from "react"; // Importar React para usar React.StrictMode
+import React from "react"; // Importar React para usar React.StrictMode y React.Fragment
+import { Toaster } from "@/components/ui/toaster"; // Importar Toaster
+import { Toaster as Sonner } from "@/components/ui/sonner"; // Importar Sonner
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <React.Fragment> {/* Este Fragment permite múltiples hijos para StrictMode */}
+      <App />
+      <Toaster />
+      <Sonner />
+    </React.Fragment>
   </React.StrictMode>
 );
