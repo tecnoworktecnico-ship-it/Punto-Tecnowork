13d3dd2c9acd2912b4daa3a8236803995f6d84d3
[dyad] <git@dyad.sh>
2025-12-02 09:51:29 -0300

    [dyad] Reintroduciendo SessionContextProvider para resolver el error de contexto. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom"; // Importar BrowserRouter, Routes, Route
-import LandingPage from "./pages/LandingPage"; // Importar LandingPage
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import { SessionContextProvider } from "./contexts/SessionContext"; // Importar SessionContextProvider
 
 const queryClient = new QueryClient();
 
@@ -11,11 +12,12 @@ const App = () => (
   <React.Fragment>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        {/* BrowserRouter espera un único hijo, que será Routes */}
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          {/* Agrega otras rutas aquí si es necesario para probar, pero por ahora, solo la principal */}
-        </Routes>
+        <SessionContextProvider> {/* Envolver BrowserRouter con SessionContextProvider */}
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            {/* Agrega otras rutas aquí si es necesario */}
+          </Routes>
+        </SessionContextProvider>
       </BrowserRouter>
     </QueryClientProvider>
     <Toaster />
