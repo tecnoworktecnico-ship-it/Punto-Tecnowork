725bcb1c38aa06bbe78865db45909a6aa2f73abb
[dyad] <tecnoworktecnico@gmail.com>
2025-12-20 11:31:16 -0300

    Reverted all changes back to version 3fb4675efbe442679a12cfec4a6ff1ee0081551c

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -6,7 +6,6 @@ import { useSession } from "@/contexts/SessionContext";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import BrandingDisplay from "@/components/BrandingDisplay";
-import Footer from "@/components/Footer"; // Importar Footer
 
 const LandingPage = () => {
   const { loading, session } = useSession();
@@ -81,8 +80,6 @@ const LandingPage = () => {
           Iniciar Sesión / Registrarse
         </Button>
       </div>
-      {/* El Footer contiene los logos Powered By */}
-      <Footer /> 
       <MadeWithDyad />
     </div>
   );
